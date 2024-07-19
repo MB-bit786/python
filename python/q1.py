acdb5f4aaf5d1ce4b73f783d9ca99a108d94fa5c
MOHAMMED BOOKWALA <114876033+MB-bit786@users.noreply.github.com>
2024-07-19 09:30:54 +0530

    Add files via upload

---
+++ python/q1.py
@@ -0,0 +1,9 @@
+"""Write a Python program to check if a number is positive,
+negative or zero."""
+num = int(input("Enter num:"))
+if num > 0:
+    print( "it is postive")
+elif num < 0:
+      print(" it is negative")
+else:
+    print("it is zero")
---
+++ python/q10.py
@@ -0,0 +1,5 @@
+#• Write a Python program to calculate the length of a string.
+
+
+string = input("Enter a string: ")
+print(len(string))
